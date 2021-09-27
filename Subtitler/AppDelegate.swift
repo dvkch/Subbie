af672738903d10da375b0b3335964faa260779eb
Stanislas Chevallier <contact@stanislaschevallier.fr>
2021-09-28 01:05:43 +0200

    Initial Commit

---
+++ Subtitler/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Subtitler
+//
+//  Created by Stanislas Chevallier on 28/09/2021.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
